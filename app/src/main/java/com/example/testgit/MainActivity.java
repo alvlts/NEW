de7984bc6deedc9cc6afdd9776139219027cb94d
alvlts <alvlts@mail.ru>
2021-03-25 12:50:45 +0300

    Добавил тост

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.testgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // fdsagsg
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
